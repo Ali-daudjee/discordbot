865dfc111cb77a8906ee0c77d2ff6b7ddf36fa0e
Ali Daudjee <alidaudjee@Alis-MacBook-Pro.local>
2022-09-10 01:30:48 -0400

    fixed a small bug with gif command not making a single worded search the keyword for the search and defaulting to nature

--- commands/gif.js
+++ commands/gif.js
@@ -2,9 +2,10 @@ const fetch = require('node-fetch');
 module.exports = async function (message, commandArr){
     //console.log(commandArr);
     let keywords = "nature";
-    if (commandArr.length > 1) {
+    if (commandArr.length > 0) {
         keywords = commandArr.join(" ");
     }
+
         let url = `https://tenor.googleapis.com/v2/search?q=${keywords}&key=${process.env.TENORKEY}&contentfilter=high`;
         let response = await fetch(url);
         let json = await response.json();
